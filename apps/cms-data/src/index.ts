f463609642990c9cf00911ad40ae7263f3dce75a
Robert Aron <contact@robertaron.io>
2025-02-23 16:49:08 -0600

    cleanup some install stuff

--- apps/cms-data/src/index.ts
+++ apps/cms-data/src/index.ts
@@ -1,9 +1,8 @@
 import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
 import * as schema from "./schema";
-import * as relations from "./relations";
 
 export function getDb(file: string) {
   const client = createClient({ url: `file:${file}` });
-  return drizzle({ client, schema: { ...schema, ...relations } });
+  return drizzle({ client, schema: { ...schema } });
 }
