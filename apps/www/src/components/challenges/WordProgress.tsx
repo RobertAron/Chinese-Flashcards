231d722f9e34c4846a61ff30e1464c84384a572b
Robert Aron <contact@robertaron.io>
2025-03-12 00:07:00 -0500

    more punctuation, fix phrase

--- apps/www/src/components/challenges/WordProgress.tsx
+++ apps/www/src/components/challenges/WordProgress.tsx
@@ -53,8 +53,8 @@ const letterMapping: Record<string, string[]> = {
   ü: ["ǖ", "ǘ", "ǚ", "ǜ"],
 };
 
-const noTypingRequired = /[ .?’]/;
-const punctuation = /[.?’]/;
+const noTypingRequired = /[ .?？’]/;
+const punctuation = /[.?？’]/;
 function extractListenChar(char: string | ToneMapConfig | undefined) {
   const isToneCharacter = typeof char === "object";
   return ((isToneCharacter ? char.letterKey : char) ?? "").toLowerCase();
