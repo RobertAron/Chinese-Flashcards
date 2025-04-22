f30297ad3cad0205c651a89a349c5d946269732b
Robert Aron <contact@robertaron.io>
2025-04-21 22:42:08 -0500

    db fixes

--- apps/www/src/components/Kbd.tsx
+++ apps/www/src/components/Kbd.tsx
@@ -1,6 +1,6 @@
 export function Kbd({ children }: { children?: React.ReactNode }) {
   return (
-    <div className="flex rounded-sm border border-black pb-[.05em] shadow-black/50 shadow-sm group-hocus:border-white">
+    <div className="hidden rounded-sm border border-black pb-[.05em] shadow-black/50 shadow-sm group-hocus:border-white md:flex">
       {/* bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500 */}
       <kbd className="no-underline! rounded-sm border-black border-b p-0.5 px-1 text-sm leading-[1em] no-underline group-hocus:border-white">
         {children}
