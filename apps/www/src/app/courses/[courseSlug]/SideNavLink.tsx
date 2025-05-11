83566f3ebd9ded5fdf5fc3865c773559349aea46
Robert Aron <contact@robertaron.io>
2025-05-10 21:25:47 -0500

    sidenav styles fix

--- apps/www/src/app/courses/[courseSlug]/SideNavLink.tsx
+++ apps/www/src/app/courses/[courseSlug]/SideNavLink.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { Link } from "@/utils/NextNavigationUtils";
-import { useTailwindOverride } from "@/utils/styleResolvers";
 import { usePathname } from "next/navigation";
 
 type LessonLinkProps = {
@@ -10,8 +9,7 @@ type LessonLinkProps = {
   children?: React.ReactNode;
 };
 
-const coreLinkClasses = "text-gray-600 truncate hocus:text-black";
-const selectedLinkClasses = "text-gray-800 font-bold";
+const coreLinkClasses = "text-gray-600 truncate hocus:text-black data-[selected=true]:text-gray-800 data-[selected=true]:font-bold";
 
 type CourseTitleLinkProps = {
   courseSlug: string;
@@ -19,14 +17,8 @@ type CourseTitleLinkProps = {
 };
 export const CourseTitleLink = ({ courseSlug, children }: CourseTitleLinkProps) => {
   const href = `/courses/${courseSlug}`;
-  const classes = useTailwindOverride(
-    coreLinkClasses,
-    selectedLinkClasses,
-    "truncate border-black border-gray-400 border-b-1 px-1 py-2 text-2xl",
-  );
-
   return (
-    <Link href={href} className={classes}>
+    <Link href={href} className="truncate border-black border-b-1 px-1 py-2 font-bold text-2xl text-black">
       {children}
     </Link>
   );
@@ -36,11 +28,12 @@ export const LessonLink = ({ courseSlug, lessonSlug, title }: LessonLinkProps) =
   const pathname = usePathname();
   const href = `/courses/${courseSlug}/${lessonSlug}`;
   const matchingPath = pathname.startsWith(href);
-  const classes = useTailwindOverride(coreLinkClasses, "font-semibold", {
-    [selectedLinkClasses]: matchingPath,
-  });
   return (
-    <Link href={href} className={classes}>
+    <Link
+      data-selected={matchingPath}
+      href={href}
+      className={`${coreLinkClasses} font-semibold`}
+    >
       {title}
     </Link>
   );
@@ -58,11 +51,13 @@ export const DrillLink = ({ courseSlug, lessonSlug, drillSlug, title }: DrillLin
   const pathname = usePathname();
   const href = `/courses/${courseSlug}/${lessonSlug}/${drillSlug}`;
   const matchingPath = pathname.startsWith(href);
-  const classes = useTailwindOverride(coreLinkClasses, "border-[currentColor] border-l-[1.5px] pl-2", {
-    [selectedLinkClasses]: matchingPath,
-  });
   return (
-    <Link href={`/courses/${courseSlug}/${lessonSlug}/${drillSlug}`} key={drillSlug} className={classes}>
+    <Link
+      data-selected={matchingPath}
+      href={`/courses/${courseSlug}/${lessonSlug}/${drillSlug}`}
+      key={drillSlug}
+      className={`${coreLinkClasses} border-[currentColor] border-l-[1.5px] pl-2`}
+    >
       <div className="pl-2">{title}</div>
     </Link>
   );
